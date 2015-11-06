4612e944ec7c7bc1b3c41db4a812c372f08fcecf
scott.lan <scott.lan@taisys.com>
2015-11-06 18:51:26 +0800

    add send to google sample by http request

---
+++ test02.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+from scapy.all import *
+
+print HTTP_REQUEST
+
+# 先作三方握手
+syn = IP(dst='www.google.com') / TCP(dport=80, flags='S', sport=16666)
+syn_ack = sr1(syn)
+
+getStr = 'GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n'
+request = IP(dst='www.google.com') / TCP(dport=80, sport=syn_ack[TCP].dport,
+             seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='A') / getStr
+
+request.show()  # print package information
+
+reply = sr1(request)
+print [reply]
